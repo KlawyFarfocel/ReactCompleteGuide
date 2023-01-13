cadf56a0371e0303ebcd9353ff70a1bf03328b67
KlawyFarfocel <smierciak.maciej@gmail.com>
2023-01-13 18:04:21 +0100

    New README files and classes-1

---
+++ JavaScript-Refresh-ES6/Classes-1/README.md
@@ -0,0 +1,4 @@
+### Klasy
+<h1>Dziedziczenie</h1>
+<p>Klasy tworzymy za pomocą "extends" po czym następuje klasa nadrzędna</p>
+<p>Funkcja super() w konstruktorze pozwala na używanie konstruktora klasy nadrzednej</p>
---
+++ JavaScript-Refresh-ES6/Classes-1/classes.js
@@ -0,0 +1,22 @@
+class Human{
+    constructor(gender){
+        this.gender=gender;
+    }
+    PrintGender(){
+        console.log("Płeć: "+this.gender);
+    }
+}
+class Person extends Human{
+  constructor(name, age,gender) {
+    super(gender);
+    this.name = name;
+    this.age = age;
+  }
+  printPerson() {
+    console.log("Imię: " + this.name);
+    console.log("Wiek: " + this.age);
+    this.PrintGender();
+  }
+}
+const person=new Person("Maciej",21,"Mężczyzna");
+person.printPerson();
